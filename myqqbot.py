6799de42c98c235d6b5f25c9d91a4c3a42d97275
lietxia <lietxia@gmail.com>
2019-08-30 22:00:15 +0800

    init

---
+++ myqqbot.py
@@ -0,0 +1,22 @@
+from aiocqhttp import CQHttp
+
+bot = CQHttp(api_root="http://127.0.0.1:5700/")
+
+
+@bot.on_message()
+async def handle_msg(context):
+    await bot.send(context, "你好呀，下面一条是你刚刚发的：")
+    return {"reply": context["message"]}
+
+
+@bot.on_notice("group_increase")
+async def handle_group_increase(context):
+    await bot.send(context, message="欢迎新人～", at_sender=True, auto_escape=True)
+
+
+@bot.on_request("group", "friend")
+async def handle_request(context):
+    return {"approve": True}
+
+
+bot.run(host="127.0.0.1", port=12345)
